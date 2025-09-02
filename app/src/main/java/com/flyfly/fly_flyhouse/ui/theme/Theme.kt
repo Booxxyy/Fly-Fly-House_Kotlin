d0ed406bf81e5a65f57ff0c5451b173080b4109d
Booxxyy <andrent20@mail.ru>
2025-09-03 01:22:40 +0500

    Theme

--- app/src/main/java/com/flyfly/fly_flyhouse/ui/theme/Theme.kt
+++ app/src/main/java/com/flyfly/fly_flyhouse/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -12,15 +13,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PrimaryColor,
+    onPrimary = OnPrimaryColor,
+    surface = SurfaceColor,
+    onSurface = OnSurfaceColor,
+    background = BackgroundColor,
+    onBackground = OnBackgroundColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = PrimaryColor,
+    onPrimary = OnPrimaryColor,
+    surface = SurfaceColor,
+    onSurface = OnSurfaceColor,
+    background = BackgroundColor,
+    onBackground = OnBackgroundColor
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
--- app/src/main/java/com/flyfly/fly_flyhouse/ui/theme/Type.kt
+++ app/src/main/java/com/flyfly/fly_flyhouse/ui/theme/Type.kt
@@ -5,11 +5,22 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.flyfly.fly_flyhouse.R
+
+
+
+val fonts = FontFamily(
+    Font(resId = R.font.roboto_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.roboto_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.roboto_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.roboto_light, weight = FontWeight.Light)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
